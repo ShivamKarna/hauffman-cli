7a6968020d9da63f961f98495a5ae07ec5e7944c
Shivam Karn <shivamkarn767@gmail.com>
2026-08-11 18:18:25 +0545

    Command_enum : Added a command enum to pattern match error handilng of `args`.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,60 @@
 mod error;
 mod hauffman;
 
+enum Command<'a> {
+    Encode { filepath: &'a str },
+    Decode { filepath: &'a str },
+}
+
 use error::HauffmanError;
-fn main() -> Result<(), HauffmanError> {
-    let content = hauffman::read_file("/home/shivamkarn/hauffman/src/example.txt")?;
 
-    let frequencies = hauffman::count_frequencies(&content);
-
-    if let Some(root) = hauffman::build_tree(&frequencies) {
-        let codes = hauffman::generate_code(&root);
-        println!("=== HUFFMAN CODES FOR FILE ===");
-        for (ch, code) in &codes {
-            println!("{:?}: {}", ch, code);
+impl<'a> Command<'a> {
+    fn parse(args: &'a [String]) -> Result<Command<'a>, HauffmanError> {
+        match args {
+            [_, flag, filepath, ..] => match flag.as_ref() {
+                "-en" => Ok(Command::Encode { filepath: filepath }),
+                "-den" => Ok(Command::Decode { filepath: filepath }),
+                _ => Err(HauffmanError::UnknownFlag),
+            },
+            _ => Err(HauffmanError::NoArguments),
         }
     }
-    Ok(())
+
+    pub fn run(self: &Self) -> Result<(), HauffmanError> {
+        match self {
+            Command::Encode { filepath } => {
+                if !filepath.ends_with(".txt") {
+                    println!("Error: Only '.txt' files can be Encoded!");
+                    return Err(HauffmanError::InvalidHeader);
+                }
+                println!("Encoding '{}'...", filepath);
+                let content = hauffman::read_file(filepath)?;
+                let output_path = filepath.replace(".txt", ".en.hauff");
+
+                hauffman::encode_to_file(&content, &output_path)?;
+                println!("Success! File encoded to: {}", output_path);
+            }
+
+            Command::Decode { filepath } => {
+                if !filepath.ends_with(".hauff") {
+                    println!("Error: Only '.hauff' files can be Decoded!");
+                    return Err(HauffmanError::InvalidHeader);
+                }
+                println!("Decoding '{}'...", filepath);
+                let output_path = filepath.replace(".en.hauff", ".den.hauff");
+
+                hauffman::decode_to_file(filepath, &output_path)?;
+                println!("Success! File decoded to: {}", output_path);
+            }
+        }
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), HauffmanError> {
+    let args: Vec<String> = std::env::args().collect();
+
+    let command = Command::parse(&args)?;
+
+    command.run()
 }
